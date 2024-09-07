58cdfd97c63b750b29331087dc296bcf4c866bc3
kumarvarun3834 <kumarvarun.vk6338@gmail.com>
2024-09-07 08:51:36 +0530

    added templates of telegram

--- data_share_bot/copymessages.py
+++ data_share_bot/copymessages.py
@@ -1,1 +1,21 @@
 # this file will help in sending the files to bot
+import requests
+def copyMessages(baseurl,TOKEN , chat_id, from_chat_id, message_ids) -> None:
+    url=baseurl+TOKEN+"copyMessages"
+    parameters={
+        "chat_id":chat_id,
+        "from_chat_id": from_chat_id,
+        "message_id":message_ids
+                }
+    run=0
+    try:
+        res=requests.get(url,data=parameters)
+    except Exception as e:
+        print(f'error occured {e}')
+        if run<5:
+            copyMessages(baseurl,TOKEN , chat_id, from_chat_id, message_ids)
+            run+=1
+    import dataview
+    lst=dataview.data_view(res)
+    print(lst)
+    
--- data_share_bot/getupdates.py
+++ data_share_bot/getupdates.py
@@ -1,1 +1,33 @@
 # this file will get updates from the bot 
+import requests
+def get_updates(baseurl,TOKEN,offset=None, limit=15 )-> list:      
+    # THESE UPDATES RESET AFTER DAY ENDS BE SURE TO SAVE
+    url=baseurl+TOKEN+"getupdates"
+    
+    parameters={
+            "offset":offset,
+            "limit":limit
+                }
+    run=0
+    try:
+        out=requests.get(url,data=parameters)
+        print(out.status_code)
+        print("Status Code:", out.status_code)
+    except Exception as e:
+        print(f'error occured {e}')
+        if run<5:
+            get_updates(baseurl,TOKEN,offset)
+            run+=1
+    if out.status_code == 200:
+        import dataview
+        lst=dataview.data_view(out)
+        return lst
+    else:
+        print("Error fetching updates")
+        return []
+    
+# from data import *
+# while True:
+#     lst =get_updates(baseurl,TOKEN,offset=0,limit=None)
+#     for i in lst:
+#         print(i)
