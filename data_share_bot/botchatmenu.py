acf1cb9c71baa809275a0f2e77155785c53f89cc
kumarvarun3834 <kumarvarun.vk6338@gmail.com>
2024-09-07 10:10:21 +0530

    added the menu text to get menu buttons and send via text to the bot

--- data_share_bot/botchatmenu.py
+++ data_share_bot/botchatmenu.py
@@ -20,6 +20,44 @@ menu=read_menu_data("datasharebot\main_menu_buttons")
 # # print(menu["/semester1"]
 # print(list(menu.keys()))
 from data import *
+def display_semester(baseurl,TOKEN,user,update,menu_data=read_menu_data("datasharebot\main_menu_buttons")):
+    """
+    baseurl,TOKEN : requirement for working of bot
+    temp_offset : from which update id we have to read data
+    user : its the userid from which we have to get results
+    menu_data : data from where we are getting our directory
+    """
+    # this message will show user a menu of semesters that are currently stored in it
+    # Display the main menu (semesters)
+    menu_text = "Choose a semester:\n"
+    
+    for semester in menu_data:
+        menu_text += f"{semester}\n"
+
+    import send_message
+    send_message.sendMessage(baseurl,TOKEN,user,menu_text,update["message_id"])
+    print(menu_text)
+
+
+def display_subject(baseurl,TOKEN,user,semester_choice,update,menu_data=read_menu_data("datasharebot\main_menu_buttons")):
+    """
+    baseurl,TOKEN : requirement for working of bot
+    temp_offset : from which update id we have to read data
+    user : its the userid from which we have to get results
+    semester_choice : the subjects inside the semesters directory
+    menu_data : data from where we are getting our directory
+    """
+    # this message will show user a menu of semesters that are currently stored in it
+    # Display the main menu (semesters)
+    menu_text = "Choose a subject:\n"
+    for subject in (menu_data[semester_choice]):
+        menu_text += f"{subject}\n"
+
+    import send_message
+    send_message.sendMessage(baseurl,TOKEN,user,menu_text,update["message_id"])
+    print(menu_text)
+
+    
 
 user_data = {}  # Dictionary to keep track of each user's offset
 
